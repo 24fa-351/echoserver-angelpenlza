191bd2f4545d95a9d7b6beda738b7b9828bdfc3a
angel <angelpmagallon10@gmail.com>
2024-11-05 15:41:04 -0800

    multithreaded

--- serverv4.c
+++ serverv4.c
@@ -1,9 +1,8 @@
 // Author:      Angel Penaloza
-// Assignment:  Echo Server
+// Assignment:  Echo Server (with multithreading)
 // Description: Creating a server which echos all messages received, line by line.
 //              Implemented with the echo_server funciton. 
-//              Funciton returns a char* (string) data type, which specifies the status of the server.
-//              Returns success if no error, returns error message if something went wrong. 
+//              Funciton returns a void pointer data type, to accommodate for the multithreading.
 //              Takes one integer argument, which is the user specified port number to connect to.
 
 #include <stdlib.h>
@@ -12,11 +11,15 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 //---- echo server function ----//
-char* echo_server(int port) {
+void* echo_server(void* v_port) {
+    printf("enter\n");
 
     // variables
+    int* p = (int*)v_port;
+    int port = *p;
     int server_file_desc;
     int new_socket = 1;
     int opt = 1; 
@@ -26,13 +29,15 @@ char* echo_server(int port) {
 
     // create file descriptor
     server_file_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_file_desc < 0) 
-        return "error creating socket";
+    if(server_file_desc < 0) {
+        perror("error creating socket\n");
+        return NULL;
+    }
 
     // set port to given port
     if(setsockopt(server_file_desc, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        printf("port: %d\n", port - 1);
-        return "error setting socket";
+        perror("error setting the sock opt\n");
+        return NULL;
     } 
 
     // set addr values
@@ -41,17 +46,23 @@ char* echo_server(int port) {
     addr.sin_port = htons(port);
 
     // bind
-    if(bind(server_file_desc, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-        return "error binding";
+    if(bind(server_file_desc, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("error binding\n");
+        return NULL;
+    }
 
     // listen
-    if(listen(server_file_desc, 3) < 0)
-        return "error listening";
+    if(listen(server_file_desc, 3) < 0) {
+        perror("error listening\n");
+        return  NULL;
+    }
 
     // create new socket using accept function
     new_socket = accept(server_file_desc, (struct sockaddr*)&addr, &addrlen);
-    if(new_socket < 0)
-        return "error accepting";
+    if(new_socket < 0) {
+        perror("error accepting\n");
+        return NULL;
+    }
 
     // read and print in loop
     while(new_socket > 0) {
@@ -64,7 +75,7 @@ char* echo_server(int port) {
     close(new_socket);
     close(server_file_desc);
     
-    return "successful";
+    return NULL;
 }
 //---- end of echo server function ----//
 
@@ -72,7 +83,7 @@ int main(int argc, char* argv[]) {
 
     // make sure there is a valid amount of arguments
     if(argc < 2) {
-        printf("not enough arguments\n");
+        printf("insufficient arguments\n");
         exit(1);
     }
 
@@ -82,11 +93,14 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // retrieve user port
+    // variables
     int port = atoi(argv[2]);
+    pthread_t tid; 
+
 
     // run the echo server
-    printf("echo server status: %s\n", echo_server(port));
+    pthread_create(&tid, NULL, echo_server, (void*)&port);
+    pthread_join(tid, NULL);
 
     return 0;
 }
